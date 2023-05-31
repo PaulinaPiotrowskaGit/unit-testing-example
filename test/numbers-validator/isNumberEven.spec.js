74c0d1e6c8ae7b4a7bc273745a8ef49595dc695f
Paulina Piotrowska <paulina_piotrowska@epam.com>
2023-05-31 15:42:05 +0200

    fixed path

--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const { describe, beforeEach, it } = require('mocha');
-const NumbersValidator = require('/Users/Paulina_Piotrowska/Desktop/Automation in JS Self-paced Program/Automation in JS/unit-testing-example/app/numbers-validator.js');
+const NumbersValidator = require('../../app/numbers-validator.js');
 
 describe('isNumberEven', function () {
   let validator;
